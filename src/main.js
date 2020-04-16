45492159365f6f1325223eee21ddbd77c0bbad99
Marcel Maruszak <64kivvi@gmail.com>
2020-04-16 08:54:46 +0200

    firebase & vuetify

--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
 import Vue from "vue";
 import App from "./App.vue";
+import firebase from "firebase";
 import "./registerServiceWorker";
 import router from "./router";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBXnk9BiOd8aGBflXuDUJpMPHJogAHsrns",
+  authDomain: "contaz-pl.firebaseapp.com",
+  databaseURL: "https://contaz-pl.firebaseio.com",
+  projectId: "contaz-pl",
+  storageBucket: "contaz-pl.appspot.com",
+  messagingSenderId: "539165494457",
+  appId: "1:539165494457:web:3591cd06378471fe26df68",
+  measurementId: "G-RMH1D4G5YW"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
